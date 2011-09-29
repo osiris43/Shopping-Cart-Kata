776298513e283bb32bde24ba9e2e87d338ca9d7f
Brett Bim <osiris43@gmail.com>
2011-09-29 08:34:41 -0500

    refactoring to clean up repetitive code

--- Checkout.rb
+++ Checkout.rb
@@ -1,19 +1,16 @@
 class Checkout
-  
+  attr_accessor :total 
+
   def initialize(rules)
     @rules=rules
     @total=0
-    @receipt = {}
+    @receipt = Hash.new(0) 
   end
 
   def scan(item)
     if !@rules.has_key?(item)
       return
     else
-      if !@receipt.has_key?(item)
-        @receipt[item] = 0
-      end
-
       @receipt[item] += 1
       
       rule = @rules[item]
@@ -26,42 +23,28 @@ class Checkout
     end
   end
 
-  def total
-    @total
-  end
-
   def check_special?(item, rule)
-    if rule.special_price.empty?
-      return
+    if rule.special_price.nil?
+      return false
     end
 
-    special_count = rule.special_price.split(' ')[0].to_i
-    special_price = rule.special_price.split(' ')[2].to_i
-    
-    @receipt[item].remainder(special_count) == 0
+    @receipt[item].remainder(rule.special_count) == 0
     
   end
 
   def add_special(item, rule)
-    special_count = rule.special_price.split(' ')[0].to_i
-    special_price = rule.special_price.split(' ')[2].to_i
-
-    @total += special_price - (special_count- 1) * rule.unit_price
+    @total += rule.special_price - (rule.special_count - 1) * rule.unit_price
   end
 end
 
 class Rule
-
-  def initialize(unit_price,special_price)
+  attr_accessor :unit_price, :special_count, :special_price
+  def initialize(unit_price,special_description)
     @unit_price = unit_price
-    @special_price=special_price
-  end
 
-  def unit_price
-    @unit_price
-  end
-
-  def special_price
-    @special_price
+    if !special_description.empty?
+      @special_count = special_description.split(' ')[0].to_i
+      @special_price = special_description.split(' ')[2].to_i
+    end
   end
 end
